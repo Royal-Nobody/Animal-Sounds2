b66e2864bac7d83a4e59ef8f13b48200be227672
Royal-Nobody <74615786+Royal-Nobody@users.noreply.github.com>
2025-07-30 21:07:39 -0700

    Updated program

--- Animal-Sounds/Program.cs
+++ Animal-Sounds/Program.cs
@@ -1,9 +1,24 @@
-﻿namespace Animal_Sounds;
+﻿namespace AgileDev_GitPractice;
 
 class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello, World!");
+        List<Animal> animals = new List<Animal>();
+        
+        animals.Add(new Cassowary());
+        animals.Add(new TasmanianDevil());
+        animals.Add(new HoneyBadger());
+        animals.Add(new FunnelWebSpider());
+        animals.Add(new BulletAnt());
+        animals.Add(new GreatWhiteShark());
+        animals.Add(new Chimpanzee());
+        animals.Add(new Hippo());
+        animals.Add(new BlueRingedOctopus());
+        
+        foreach (Animal animal in animals)
+        {
+            animal.MakeSound();
+        }
     }
 }
